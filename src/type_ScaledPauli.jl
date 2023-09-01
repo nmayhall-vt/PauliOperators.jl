598f32d6a3ebf9af6ad4cc07b5987c6b78badd29
Nick Mayhall <nmayhall@vt.edu>
2023-09-01 17:01:58 -0400

    still trying

--- src/type_ScaledPauli.jl
+++ src/type_ScaledPauli.jl
@@ -88,30 +88,39 @@ Base.isequal(p1::ScaledPauli{T,N}, p2::ScaledPauli{T,N}) where {T,N} = isequal(p
 function Base.unique!(spv::Vector{ScaledPauli{T,N}}) where {T,N}
     sort!(spv)
 
-    i = 1
-    while i < length(spv)
-        njump = 0
-        j = i+1
-        while j < length(spv) && isequal(spv[j], spv[i])
-            println(i, " ", j)
-            spv[i] = ScaledPauli{T,N}(get_coeff(spv[i]) + get_coeff(spv[j]), phasefree(spv[i].pauli))
-            spv[j] *= 0 
-            j += 1
-            njump += 1
+    # i = 1
+    # while i < length(spv)
+    #     njump = 0
+    #     j = i+1
+    #     while j < length(spv) && isequal(spv[j], spv[i])
+    #         println(i, " ", j)
+    #         spv[i] = ScaledPauli{T,N}(get_coeff(spv[i]) + get_coeff(spv[j]), phasefree(spv[i].pauli))
+    #         spv[j] *= 0 
+    #         j += 1
+    #         njump += 1
+    #     end
+    #     i += 1 + njump
+    #     j += 1
+    #     # for j in i+1:length(spv)
+    #     #     if isless(spv[i], spv[j])
+    #     #         # spv[i+1] = spv[j]
+    #     #         i += njump
+    #     #         continue
+    #     #     elseif isequal(spv[i], spv[j])
+    #     #         njump += 1
+    #     #         spv[i] = ScaledPauli{T,N}(get_coeff(spv[i]) + get_coeff(spv[j]), phasefree(spv[i].pauli))
+    #     #     else
+    #     #         error(i, " ", j, " Why is this not sorted?")
+    #     #     end 
+    #     # end
+    # end
+    for i in 1:length(spv)-1
+        for j in i+1:length(spv)
+            if isless(spv[i], spv[j])
+                continue
+            elseif isequal(spv[i], spv[j])
+                
+            end
         end
-        i += 1 + njump
-        j += 1
-        # for j in i+1:length(spv)
-        #     if isless(spv[i], spv[j])
-        #         # spv[i+1] = spv[j]
-        #         i += njump
-        #         continue
-        #     elseif isequal(spv[i], spv[j])
-        #         njump += 1
-        #         spv[i] = ScaledPauli{T,N}(get_coeff(spv[i]) + get_coeff(spv[j]), phasefree(spv[i].pauli))
-        #     else
-        #         error(i, " ", j, " Why is this not sorted?")
-        #     end 
-        # end
     end
 end
