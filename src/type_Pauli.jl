c17ba4ce6dd95fb442ef38609defaaab5cb3353b
Nick Mayhall <nmayhall@vt.edu>
2023-09-09 13:12:35 -0400

    back to pauli again

--- src/type_Pauli.jl
+++ src/type_Pauli.jl
@@ -147,8 +147,8 @@ end
 TBW
 """
 function phasefree(p::Pauli)
-    return (p.z, p.x) 
-    # return rotate_phase(p, -p.θ)
+    # return (p.z, p.x) 
+    return rotate_phase(p, -p.θ)
 end
 
 """
--- src/type_PauliSum.jl
+++ src/type_PauliSum.jl
@@ -11,7 +11,8 @@ This uses a `Dict` to store them, however, the specific use cases should probabl
 so this will probably be removed.
 """
 struct PauliSum{N}  
-    ops::Dict{PauliIndex,ComplexF64}
+    ops::Dict{Pauli{N},ComplexF64}
+    # ops::Dict{PauliIndex,ComplexF64}
 end
 
 
@@ -21,7 +22,8 @@ end
 TBW
 """
 function PauliSum(N)
-    return PauliSum{N}(Dict{PauliIndex,ComplexF64}())
+    return PauliSum{N}(Dict{Pauli{N},ComplexF64}())
+    # return PauliSum{N}(Dict{PauliIndex,ComplexF64}())
 end
 
 
@@ -32,8 +34,8 @@ TBW
 """
 function Base.display(ps::PauliSum{N}) where N
     for (key,val) in ps.ops
-        # @printf(" %12.8f +%12.8fi θ:%1i %s\n", real(val), imag(val), key.θ, key)
-        @printf(" %12.8f +%12.8fi %s\n", real(val), imag(val), string(Pauli{N}(0,key[1],key[2])))
+        @printf(" %12.8f +%12.8fi θ:%1i %s\n", real(val), imag(val), key.θ, key)
+        # @printf(" %12.8f +%12.8fi %s\n", real(val), imag(val), string(Pauli{N}(0,key[1],key[2])))
     end
 end
 
@@ -42,7 +44,7 @@ Base.keys(ps::PauliSum) = keys(ps.ops)
 Base.getindex(ps::PauliSum{N}, p::PauliIndex) where N = ps.ops[p]
 Base.getindex(ps::PauliSum{N}, p::Pauli{N}) where N = ps.ops[phasefree(p)]
 Base.setindex!(ps::PauliSum{N}, v, p::Pauli{N}) where N = ps.ops[phasefree(p)] = v*get_phase(p)
-Base.setindex!(ps::PauliSum{N}, v, p::PauliIndex) where N = ps.ops[p] = v
+# Base.setindex!(ps::PauliSum{N}, v, p::PauliIndex) where N = ps.ops[p] = v
 
 """
     Base.+(p1::PauliSum{N}, p2::PauliSum{N}) where {N}
