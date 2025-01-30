6557c2d1b00dd2902911ac3b5b087e241eaeb62e
Nick Mayhall <nmayhall@vt.edu>
2025-01-30 13:51:31 -0500

    added tests

--- test/test_operator_methods.jl
+++ test/test_operator_methods.jl
@@ -139,24 +139,36 @@ end
 
 @testset "Tensor product" begin
     N = 3
-    types = []
-    push!(types, PauliBasis{N})
-    push!(types, Pauli{N})
-    push!(types, PauliSum{N, ComplexF64})
-    push!(types, DyadBasis{N})
-    push!(types, Dyad{N})
-    push!(types, DyadSum{N, ComplexF64})
+    types1 = []
+    push!(types1, PauliBasis{N})
+    push!(types1, Pauli{N})
+    push!(types1, PauliSum{N, ComplexF64})
+    push!(types1, DyadBasis{N})
+    push!(types1, Dyad{N})
+    push!(types1, DyadSum{N, ComplexF64})
+    
+    N = 2
+    types2 = []
+    push!(types2, PauliBasis{N})
+    push!(types2, Pauli{N})
+    push!(types2, PauliSum{N, ComplexF64})
+    push!(types2, DyadBasis{N})
+    push!(types2, Dyad{N})
+    push!(types2, DyadSum{N, ComplexF64})
 
     @test Pauli("X") ⊗ Pauli("Y") ⊗ Pauli("Z") == Pauli("XYZ")
-    for T in types
+    for Ti in 1:length(types1)
         for i in 1:100
-            a = rand(T)
-            b = rand(T)
+            T1 = types1[Ti]
+            T2 = types2[Ti]
+            a = rand(T1)
+            b = rand(T2)
             err = norm(kron(Matrix(b), Matrix(a)) - Matrix(a⊗b)) < 1e-14
             if !err
                 @show T, err
             end
             @test err
+            
         end 
         
     end
